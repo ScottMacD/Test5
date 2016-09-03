a5eca75777f3a8298c417fddc30d1f0ab3095272
ScottMacD <ScottMacD82@gmail.com>
2016-09-02 22:32:57 -0500

    Integer to Text - Demo

---
+++ src/Test5.java
@@ -0,0 +1,11 @@
+
+
+public class Test5 {
+	public static void main(String[] args) {
+        Test4 testObj = new Test4();
+        System.out.println(testObj.convertValue(5454545));
+        System.out.println(testObj.convertValue(-78));
+        System.out.println(testObj.convertValue(458));
+
+    }
+}
